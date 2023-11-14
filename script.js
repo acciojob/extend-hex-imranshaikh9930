40f195ce83c3c546be1ebd872db9206cdaa4d700
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-14 14:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const hexWithoutHash = shortHex.startsWith('#') ? shortHex.slice(1) : shortHex;
+
+    // Double each character to get the full hex code
+    const fullHex = hexWithoutHash
+        .split('')
+        .map(char => char + char)
+        .join('');
+
+    // Add the # prefix to the full hex code
+    return '#' + fullHex;
 };
 
 // Do not change the code below.
